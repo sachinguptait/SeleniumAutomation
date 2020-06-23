81e4704d16d672d2ebecd8cd5c051907bd8be808
sachinguptait <sachinbilsi@gmail.com>
2020-06-23 13:39:11 +0530

    updated version of dependency and added Guava dependency

--- src/main/java/com/selenium/library/ChromeBaseClass.java
+++ src/main/java/com/selenium/library/ChromeBaseClass.java
@@ -1,23 +1,17 @@
 package com.selenium.library;
 
-import java.util.concurrent.TimeUnit;
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeTest;
-
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class ChromeBaseClass {
 	
-	public ChromeDriver driver;
+	public WebDriver driver;
 	public int lessWait=10;
 	public int mediumWait=20;
 	public int highWait=60;
 	
 	
-	@BeforeClass
 	public void driverSetUP()
 	{
 		WebDriverManager.chromedriver().setup();
